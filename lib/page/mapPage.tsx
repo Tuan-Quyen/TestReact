e837a97d5fcf1a79300bf5697d23cf61bee6294e
Long Vo <vthanhlong@tma.com.vn>
2023-04-14 16:40:14 +0700

    Installation Mapbox for Android

--- lib/page/mapPage.tsx
+++ lib/page/mapPage.tsx
@@ -1,20 +1,30 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { StyleSheet, View } from "react-native"
-
+import Mapbox from "@rnmapbox/maps";
 
 const MapPage = () => {
+    useEffect(() => {
+        Mapbox.setConnected(true);
+    })
+
     return (
         <View style={styles.container}>
+            <Mapbox.MapView
+                style={styles.map}>
+            </Mapbox.MapView>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
         flexDirection: "column",
-        justifyContent: 'center',
-        alignItems: 'center'
+        flex: 1,
+        alignItems: "stretch",
+        justifyContent: 'center'
+    },
+    map: {
+        flex: 1
     }
 })
 
