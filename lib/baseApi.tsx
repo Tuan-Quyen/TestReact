d65e18a184eb63142cdd9a4e13e9526d660cc76f
Quyen Chau <ctuanquyen97@gmail.com>
2023-04-15 14:35:53 +0700

    Update API Mapbox key

--- lib/baseApi.tsx
+++ lib/baseApi.tsx
@@ -1,3 +1,3 @@
 export const BASE_URL = "https://api.themoviedb.org/3/";
 export const API_KEY = "ee8cf966d22254270f6faa1948ecf3fc";
-export const MAP_ACCESS_KEY = 'pk.eyJ1IjoiY3RxdXllbiIsImEiOiJjbGdnMTR2MWUwNzdzM2l0MWU2ZDg3b21lIn0.7JwBC9A1Eigw2HAi4yKe3w';
+export const MAP_ACCESS_KEY = 'sk.eyJ1IjoiY3RxdXllbiIsImEiOiJjbGdobml0ZW8wbmZyM3Fycm9vMHp2ajlrIn0.r1NgEVxQwtJcK7iGQG1enQ';
